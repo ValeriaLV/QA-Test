e7afd5ad16be54f7d269b7f603431399e42b14fa
ValeriaLV <52280877+ValeriaLV@users.noreply.github.com>
2020-04-05 14:53:42 +0300

    Add files via upload

---
+++ Часть 3.lua
@@ -0,0 +1,15 @@
+
+
+function addition (a, b)
+    return a + b
+end
+
+function multiply (a, b)
+    return a * b
+end
+
+function divide (a, b)
+    if b == 0 then error("Divide by zero") end
+    
+    return a / b
+end
